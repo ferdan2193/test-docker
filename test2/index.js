da2939ece929cf675d9fe5e35e187530f3b36fa5
cierodev <67662218+cierodev@users.noreply.github.com>
2021-12-23 11:04:31 -0500

    test

---
+++ test2/index.js
@@ -0,0 +1,9 @@
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if (myTimer.IsPastDue)
+    {
+        context.log('JavaScript is running late! Fernando Alvarez');
+    }
+    context.log('JavaScript timer trigger function ran Fernando Alvarez!', timeStamp);   
+};
